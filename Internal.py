918c8517f17d9628d913b3fa003b7e40df291363
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-20 12:32:24 +1200

    Defined table, rows estabilished, specfications established

--- Internal.py
+++ Internal.py
@@ -1,7 +1,17 @@
 import sqlite3
 SQLDATABASE = 'Cars.db'
 
-db = sqlite3.connect(SQLDATABASE)
-cursor = db.cursor()
-sql = "Select * from Cars;"
-cursor.execute(sql)
+
+def create_table():
+    db = sqlite3.connect(SQLDATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM Cars;"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    print(rows)
+    print('ID       Name        Top Speed')
+
+    for row in rows:
+        print(f"Row: {row[1]} {row[2]: > 200}")
+
+    db.close()
