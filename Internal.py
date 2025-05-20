ea8736dd37ab60146e0875bc7c5d5e9e29254e4a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-20 12:38:32 +1200

    added While True loop , inpurs, and editing of the code

--- Internal.py
+++ Internal.py
@@ -15,3 +15,23 @@ def create_table():
         print(f"Row: {row[1]} {row[2]: > 200}")
 
     db.close()
+
+while True:
+    userinput = input('What would you like to do:\n print all cars:\n Exit:\n')
+    if userinput == '1':
+        create_table()
+    elif userinput == '2':
+        print('Exiting...........')
+        break
+    else:
+        print('Invalid input. Please enter a number from either 1 or 2!')
+        db.close()
+
+
+
+
+
+
+
+
+
