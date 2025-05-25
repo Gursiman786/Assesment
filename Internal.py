7935f59b6977558f924e2a68b87be0bbce604f44
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-26 10:03:10 +1200

    Added the rest of the tables as OUTPUTS

--- Internal.py
+++ Internal.py
@@ -16,11 +16,52 @@ def create_table():
 
     db.close()
 
+def create_table2():
+    db = sqlite3.connect(SQLDATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM Manufacturer;"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    print(rows)
+    print('ID       Name of manufacturer')
+
+    for row in rows:
+        print(f"Row: {row[1]} {row[2]}")
+
+    db.close()
+
+def create_table3():
+    db = sqlite3.connect(SQLDATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * FROM Engine;"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    print(rows)
+    print('ID       Engine Type')
+
+    for row in rows:
+        print(f"Row: {row[1]} {row[2]}")
+
+    db.close()
+
 while True:
-    userinput = input('What would you like to do:\n print all cars: {1}\n Exit:{2}\n')
+    userinput = input('What would you like to do:\n print all cars: {1}\n  print all Manufacturers:{2}\n print all Engines:{3}\n Exit:{4}\n')
     if userinput == '1':
         create_table()
-    elif userinput == '2':
+
+
+        userinput = input('What would you like to do:\n print all cars: {1}\n  print all Manufacturers:{2}\n print all Engines:{3}\n Exit:{4}\n')
+    if userinput == '2':
+        create_table2()
+
+
+        userinput = input('What would you like to do:\n print all cars: {1}\n  print all Manufacturers:{2}\n print all Engines:{3}\n Exit:{4}\n')
+    if userinput == '3':
+        create_table3()
+
+
+        userinput = input('What would you like to do:\n print all cars: {1}\n  print all Manufacturers:{2}\n print all Engines:{3}\n Exit:{4}\n')
+    elif userinput == '4':
         print('Exiting...........')
         break
     else:
